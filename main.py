9b9691ed2ec9ae8e0526bfa4d8277987dc8f7007
csiejar <87930236+LawrenceLee0113@users.noreply.github.com>
2022-05-09 05:05:44 +0000

    05091305

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #設定非必要不要動
-#如果被速率限制(error429)那就在shell打kill 1
+#若被速率限制error429 則在shell打kill 1
+
 from platform import java_ver
 import discord
 from discord.ext import commands
 import json
 import random,os,asyncio
+import time
 
 with open("settings.json","r",encoding="utf8") as jfile:
     jdata = json.load(jfile)
@@ -36,11 +38,25 @@ async def on_member_remove(member):
 
 #找人打瓦
 @bot.event
-async def on_message(valorant):
-    if valorant.content in jdata["瓦羅蘭"]:
-       if valorant.author != bot.user:
-        await valorant.channel.send(f"上線啊你各位<@&913401379865899018>")
-    await bot.process_commands(valorant)
+async def on_message(message):
+    if message.content in jdata["瓦羅蘭"]:
+       if message.author != bot.user:
+        await message.channel.send(f"上線啊你各位<@&913401379865899018>")
+    await bot.process_commands(message)
+    if message.content.startswith('更改狀態'):
+        #切兩刀訊息
+        tmp = message.content.split(" ",2)
+        #如果分割後串列長度只有1
+        if len(tmp) == 1:
+            await message.channel.send("你要改成什麼啦？")
+        else:
+            game = discord.Game(tmp[1])
+            #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+            await bot.change_presence(status=discord.Status.idle, activity=game)
+    if message.content in jdata["R6"]:
+        if message.author != bot.user:
+            await message.channel.send(f"欸打R6<@&911531657771773982>")
+
 
 
 #指令區
@@ -65,10 +81,22 @@ async def rex(ctx):
 async def 課表(ctx):
     await ctx.send(jdata["課表"])
 
+#傑哥
+@bot.command()
+async def 傑哥(ctx):
+    await ctx.send(jdata["傑哥"])
+  
 #dice
 @bot.command()
 async def dice(ctx):
     await ctx.send(f"1-6選出來的隨機數字為"+random.choice(jdata["dice"]))
 
+#delete msg
+@bot.command()
+async def clean(ctx,num:int):
+    await ctx.channel.purge(limit=num+1)
+    await ctx.send("清理完成")
+    time.sleep(5)
+    await ctx.channel.purge(limit=1)
 
 bot.run(jdata["TOKEN"])
